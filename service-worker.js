ae917562aff230498648957c1a7688bc5f971958
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-09-14 11:02:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.7ccaf259866948985f6958d3e5026f39.js"
 );
 
 self.addEventListener('message', (event) => {
